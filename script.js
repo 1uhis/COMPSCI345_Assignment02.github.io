c0f7476c8b5152e687470d140341029a79f6fccf
1uhis <106042973+1uhis@users.noreply.github.com>
2022-08-15 22:22:46 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿const  display = () =>{
+    document.getElementById('ct-form').style.display='block'
+    console.log("working");
+}
+
+
+const dispear = () => {
+    document.getElementById('ct-form').style.display='none'
+}
